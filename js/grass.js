c32a906b5f68a00c739888bae6570b015354dcc4
Mary <marycbush@gmail.com>
2013-08-25 01:21:10 -0700

    Added grass (and a third level)

---
+++ js/grass.js
@@ -0,0 +1,14 @@
+// ========================================= Grass
+
+function makeGrass(level, pos, size) {
+  var grass = makeLevelObject(level);
+  grass.pos = pos;
+  grass.size = size;
+  grass.drawActions.push(function() {
+    circle(game.bg_ctx, this.pos, this.size, colors.grass);
+  });
+  grass.tickActions.push(function() {
+    if (distance(ball.pos, this.pos) < this.size) {  ball.scaleVelocity(grass_speed_boost); }
+  });
+  return grass;
+}
